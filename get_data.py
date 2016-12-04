2be90f1c10bdc6ad45f4cef84b616cbef92eab4b
Jason Tong <jktong@mit.edu>
2016-12-04 15:41:23 -0500

    Add option to pad both note and note value parts of the vector by one to handle trailing non-notes in a fixed-length sequence of notes

--- get_data.py
+++ get_data.py
@@ -17,9 +17,10 @@ WINDOW = np.blackman(CHUNK)
 def midinote_to_freq(midinote):
     return C0*2**(midinote/12.0)
 
-def play_melody(melody_matrix):
+def play_melody(melody_matrix, pad=False):
+    note_range = 129 if pad else 128
     midinotes = melody_matrix.argmax(axis=1)
-    notevalues = melody_matrix[:,128:].argmax(axis=1)
+    notevalues = melody_matrix[:,note_range:].argmax(axis=1)
     frequencies = midinote_to_freq(midinotes)
     wavedata = ''
     count = 0
@@ -61,7 +62,7 @@ def get_pieces_by_ensemble(ensemble):
     print '{} pieces found!'.format(len(pieces))
     return pieces
 
-def get_piece_melody(note_tree):
+def get_piece_melody(note_tree, pad=False):
     notes = []
     begin = note_tree.begin()
     end = note_tree.end()
@@ -72,27 +73,35 @@ def get_piece_melody(note_tree):
         start,next_begin,(_,note,_,_,note_value) = highest_note
         notes.append((note, note_value))
         next_intervals = note_tree.search(next_begin, end, strict=True)
-    melody_matrix = notes_to_matrix(notes)
+    melody_matrix = notes_to_matrix(notes, pad=pad)
     return melody_matrix
 
-def notes_to_matrix(notes):
-    melody_matrix = np.zeros((len(notes),128 + len(NOTEVALUE_INDEX)))
+def notes_to_matrix(notes, pad=False):
+    note_range = 129 if pad else 128
+    note_value_range = len(NOTEVALUE_INDEX) + int(pad)
+    melody_matrix = np.zeros((len(notes),note_range + note_value_range))
     for i, note in enumerate(notes):
         melody_matrix[i][note[0]] = 1
-        melody_matrix[i][128 + 
+        melody_matrix[i][note_range + 
                          NOTEVALUE_INDEX.get(note[1],
                                              NOTEVALUE_INDEX['Quarter'])] = 1
     return melody_matrix
 
 
-if __name__=='__main__':
+def write_melody_matrices(pad=False):
+    print 'Padding set to {}!'.format(pad)
+    outfile_name = 'Data/musicnet_pianosolos{}.npz'.format('_padded' if pad else '')
+    print 'Writing to {}...'.format(outfile_name)
     piano_solos = get_pieces_by_ensemble('Solo Piano')
     melodies = {}
     count = 0
     for piece in piano_solos:
         _, note_tree = all_data[piece]
-        melodies[piece] = get_piece_melody(note_tree)
+        melodies[piece] = get_piece_melody(note_tree, pad)
         count += 1
         print '{}/{} pieces done...'.format(count, len(piano_solos))
-    with open('Data/musicnet_pianosolos.npz', 'w') as outfile:
+    with open(outfile_name, 'w') as outfile:
         np.savez(outfile, **melodies)
+
+if __name__=='__main__':
+    write_melody_matrices(True)
